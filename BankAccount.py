e05eace6fc9606781b8aeda5623aa898c152d9bc
AdithiD <adithidk1502@gmail.com>
2024-05-30 06:33:23 +0530

    Add files via upload

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,9 @@
-
 import re
 
+search1=[]
+search2=[]
+search3=[]
+search4=[]
 print("ABC BANK FORM")
 print("Please fill in up the details ")
 accountexistance = input("Do you have an account in this bank (yes/no) : ")
@@ -8,7 +11,7 @@ if accountexistance.isdigit() :
     print("It seems like you entered a number instead of yes or no So please try again!!")
 elif accountexistance.lower() == "yes" or accountexistance.lower() == "y":
     print("Ok, please fill in the details to verify!!")
-    with open("Accountdetails.txt" , "r")as file:
+    with open("AccountDetails.txt" , "r")as file:
         read = file.read()
         name = input("Enter full name : ")
         accountnum = input("Enter account number : ")
@@ -16,14 +19,17 @@ elif accountexistance.lower() == "yes" or accountexistance.lower() == "y":
         validAN = r"\d{8,12}"
         validMN = r"\d{10}"
         balance = r'\₹([0-9,]+\.\d{2})'
-        search1 = re.findall(validAN , accountnum)
-        search2 = re.findall(validMN, mobilenum)
-        search3 = re.findall(balance , read)
-        if search1  and search2 and search3:
-            print("Name : " , name)
-            print("Account Number : ",accountnum)
-            print("Mobile Number : +91",mobilenum)
-            print("Balance :₹ ",search3[0])
-        else:
-            print("Invalid Input")
+        validname = r"\w[A-Za-z]"
+        search1 = re.findall(validAN , read)
+        search2 = re.findall(validMN, read)
+        search4 = re.findall(validname , read)
+for i in range (0,len(search1),1):    
+    if accountnum == search1[i] and mobilenum == search2[i] and name == search4[i]:
+        print("Name : " , search4)
+        print("Account Number : ",search1)
+        print("Mobile Number : +91",search2)
+        print("Balance :₹ ",search3)
+    else:
+        print("Invalid Input")
+
 
